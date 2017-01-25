625e627b82b5113ad5cafa5fdee11710f29e1c40
rafae <rafae@10.12.1.64>
2017-01-25 08:28:03 -0200

    Startingo developing CurrencyControllerTest

--- src/test/java/com/rafaelbezerra/cwi/currency/test/controller/CurrencyControllerTest.java
+++ src/test/java/com/rafaelbezerra/cwi/currency/test/controller/CurrencyControllerTest.java
@@ -1,5 +1,28 @@
 package com.rafaelbezerra.cwi.currency.test.controller;
 
+import java.math.BigDecimal;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.rafaelbezerra.cwi.currency.controller.CurrencyController;
+import com.rafaelbezerra.cwi.currency.exception.CurrencyValueLessThanZeroException;
+
 public class CurrencyControllerTest {
+
+	private CurrencyController currencyController;
+
+	@Before
+	public void init() {
+		currencyController = new CurrencyController();
+
+	}
+
+	@Test(expected = CurrencyValueLessThanZeroException.class)
+	public void testValueLessThanZeroException() {
+		currencyController.currencyCalculation(null, null, new BigDecimal(-1));
+	}
+	
+	
 
 }
